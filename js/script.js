77b841dbd6c5853c3eaeab59324629803567d432
WashingtonYandun <washington.yandun06@gmail.com>
2022-05-28 23:47:40 -0500

    have to think about this, but i already make this more similar to the final result

--- js/script.js
+++ js/script.js
@@ -5,24 +5,29 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+ctx.translate(canvas.width / 2, canvas.height / 2); // make a new Origin reference
+ctx.moveTo(canvas.width / 2, canvas.height / 2); // move to new Origin
+
 // stars for the starfield
 const stars = [];
 
 //functions
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
 
-function getRandomFloat(min, max) {
+const getRandomFloat = (min, max) => {
     return Math.random() * (max - min + 1) + min;
-}
+};
+
+const getRandomColor = () => {};
 
 // class star for making dots in the screen
 class Star {
-    constructor(x, y, z, radius, color) {
+    constructor(x, y, radius, color) {
         this.x = x;
         this.y = y;
-        this.z = z; // just the iluttion of 3D (2d project)
+        this.z = 1800; // just the iluttion of 3D (2d project) from x to a new value
         this.radius = radius;
         this.color = color;
     }
@@ -33,32 +38,38 @@ class Star {
         ctx.fill();
     }
     update() {
-        this.draw();
+        this.z = this.z - 1;
+        if (this.z <= 0) {
+            this.z = 1800;
+        }
+        let newX = this.x / this.z;
+        let newY = this.y / this.z;
+        ctx.beginPath();
+        ctx.arc(newX, newY, this.radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
     }
 }
 
-const star1 = new Star(canvas.width / 2, canvas.height / 2, 10, 10, "#1f1");
-
-// recursive function for render the animation
-const animate = () => {
-    stars.forEach((star) => {
-        star.draw();
-    });
-    requestAnimationFrame(animate);
-};
-
 const init = () => {
-    for (let index = 0; index < 50; index++) {
+    for (let index = 0; index < 200; index++) {
         stars.push(
             new Star(
-                getRandomFloat(0, canvas.width),
-                getRandomFloat(0, canvas.height),
-                10,
+                getRandomFloat(-canvas.width, canvas.width),
+                getRandomFloat(-canvas.height, canvas.height),
                 getRandomInt(2, 6),
                 "#000"
             )
         );
     }
+};
+
+// recursive function for render the animation
+const animate = () => {
+    requestAnimationFrame(animate);
+    stars.forEach((star) => {
+        star.update();
+    });
 };
 
 init();
